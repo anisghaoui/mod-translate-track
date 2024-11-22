e1a3407d0df760548d9d7c449f8d109071848644
Anis <anis.ghaoui@vedecom.fr>
2024-11-22 12:05:18 +0100

    changed how keys lists are built. now they guarantee the output order.

--- compare_tools/compare_configs.py
+++ compare_tools/compare_configs.py
@@ -15,10 +15,11 @@ def compare_configs(cfg_previous: str, cfg_new: str):
     cfg_new = parse_config(cfg_new)
 
     # Find sections or keys that have changed, were added, or removed
-    added_sections = cfg_new.keys() - cfg_previous.keys()
-    removed_sections = cfg_previous.keys() - cfg_new.keys()
-    common_sections = cfg_previous.keys() & cfg_new.keys()
-
+    cfg_prev_keys = cfg_previous.keys()
+    cfg_new_keys = cfg_new.keys()
+    added_sections = cfg_new_keys - cfg_prev_keys
+    removed_sections = cfg_prev_keys - cfg_new_keys
+    common_sections = [key for key in cfg_prev_keys if key in cfg_new_keys]
     # Store differences
     diff_result = {
         "added_sections": {section: cfg_new[section] for section in added_sections},
@@ -54,51 +55,6 @@ def compare_configs(cfg_previous: str, cfg_new: str):
     return diff_result
 
 
-def compare_configs(cfg_previous: str, cfg_new: str):
-    cfg_previous = parse_config(cfg_previous)
-    cfg_new = parse_config(cfg_new)
-
-    # Find sections or keys that have changed, were added, or removed
-    added_sections = cfg_new.keys() - cfg_previous.keys()
-    removed_sections = cfg_previous.keys() - cfg_new.keys()
-    common_sections = cfg_previous.keys() & cfg_new.keys()
-
-    # Store differences
-    diff_result = {
-        "added_sections": {section: cfg_new[section] for section in added_sections},
-        "removed_sections": {section: cfg_previous[section] for section in removed_sections},
-        "common_sections": {}
-    }
-
-    # Compare keys within common sections
-    for section in common_sections:
-        added_keys = cfg_new[section].keys() - cfg_previous[section].keys()
-        removed_keys = cfg_previous[section].keys() - cfg_new[section].keys()
-        modified_keys = {
-            key for key in cfg_previous[section].keys() & cfg_new[section].keys()
-            if cfg_previous[section][key] != cfg_new[section][key]
-        }
-        unmodifed_keys = {
-            key for key in cfg_previous[section].keys() & cfg_new[section].keys()
-            if cfg_previous[section][key] == cfg_new[section][key]
-        }
-
-        # If any exists, create a dict
-        if added_keys or removed_keys or modified_keys or unmodifed_keys:
-            diff_result["common_sections"][section] = {
-                "added_keys": {key: cfg_new[section][key] for key in added_keys},
-                "removed_keys": {key: cfg_previous[section][key] for key in removed_keys},
-                "modified_keys": {
-                    key: {"from": cfg_previous[section][key],
-                          "to": cfg_new[section][key]}
-                    for key in modified_keys
-                },
-                "unmodifed_keys": {key: cfg_previous[section][key] for key in unmodifed_keys},
-            }
-
-    return diff_result
-
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('mod_url')
@@ -106,5 +62,4 @@ if __name__ == "__main__":
 
     mod = Mod(args.mod_url)
     path_previous = mod.path_en / "locale.cfg"
-    path_new = mod.download_locale_en()
-    diff = compare_configs(path_previous, path_new)
+    diff = compare_configs(path_previous, path_previous)
--- compare_tools/format_output.py
+++ compare_tools/format_output.py
@@ -4,13 +4,20 @@ from compare_configs import compare_configs
 
 from configparser import ConfigParser
 
+TEXT_DISPLAYED = {
+    "added_keys": "################## These keys were added since last update ##################",
+    "removed_keys": "These keys were removed since last update",
+    "modified_keys": "These keys were updated since last update",
+    "unmodified_keys": "################## These keys were unchanged ##################",
+}
+
 
 def format_output(config_json, mod):
     config_out = ConfigParser()
 
     # add_sections = config["added_sections"]
     # removed_sections = config["removed_sections"]
-    common_sections = config_json["common_sections"]
+    common_sections = config_json["modified_sections"]
 
     # Write the file section by section starting with the common ones
     path = "output/" + mod.name + "/locale.cfg"
@@ -18,15 +25,21 @@ def format_output(config_json, mod):
         for section_name in common_sections.keys():
             config_out.add_section(section_name)
             section = common_sections[section_name]
-            for k, v in section.items():
-                print(k)
-                print(v)
-                if len(v) != 0:
-                    for kk, vv in v.items():
+            f.write("modified_sections" + "\n")
+            for sub_key, value in section.items():
+                # key_kind is either: added_keys, removed_keys, modified_keys, unmodified_keys
+                if len(value) != 0:
+                    config_out.set(section_name,  "# " + sub_key,
+                                   TEXT_DISPLAYED[sub_key])
+
+                    for kk, vv in value.items():
+                        if sub_key == "modified_keys":
+                            # it is written as "from", "to" keys
+                            vv = vv["to"]
                         config_out.set(section_name, kk, vv)
 
-    with open('output.cfg', 'w') as cf:
-        config_out.write(cf)
+            config_out.write(f)
+            config_out = ConfigParser()
 
 
 if __name__ == "__main__":
@@ -36,8 +49,8 @@ if __name__ == "__main__":
 
     mod = Mod(args.mod_url)
     path_previous = mod.path_en / "locale.cfg"
+    path_new = mod.path_en / "locale copy.cfg"
     # path_new = mod.download_locale_en()
 
-    diff = compare_configs(path_previous, path_previous)
-
+    diff = compare_configs(path_previous, path_new)
     format_output(diff, mod)
